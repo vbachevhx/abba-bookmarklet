e5f492bd51f0e7f9cb5f119948c9fc11d6c3a389
Vasil Bachev <vasil.bachev@holidayextras.com>
2017-03-24 11:11:41 +0200

    Migrating to an HX repo

--- src/abba_bookmarklet.js
+++ src/abba_bookmarklet.js
@@ -1,8 +1,8 @@
 // Older versions of the bookmarklet will request this file. We use it to get
 // them to update while not preventing them from using the tool
 (function(){
-  alert('There is a newer version of the ABBA bookmarklet. Please update!');
+  console.warn('There is a newer version of the ABBA bookmarklet. Please update at https://github.com/holidayextras/abba-bookmarklet');
   var scriptTag = document.createElement('script');
-  scriptTag.setAttribute('src', 'https://rawgit.com/vbachevhx/abba-bookmarklet/master/dist/abba_bookmarklet.js');
+  scriptTag.setAttribute('src', 'https://tech.holidayextras.co.uk/abba-bookmarklet/dist/abba_bookmarklet.js');
   document.body.appendChild(scriptTag);
 })();
